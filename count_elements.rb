419393ded3bdc94c1f210b360d9dafee8bf1927c
Oliver <oliver.grant@gmail.com>
2019-03-12 06:00:33 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -2,11 +2,6 @@ def count_elements(array)
   # code goes here
   new_hash = {}
   array.each |animal|
-    if new_hash[animal]
-      new_hash[animal] += 1
-    else
-      new_hash[animal] = 1
-    end
   end
   new_hash
 end
